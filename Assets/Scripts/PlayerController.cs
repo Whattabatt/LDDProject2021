055001a58358cfed96b0d03f9afefd30456af29b
Whattabatt <david.l.graham1@gmail.com>
2021-04-22 20:07:30 -0700

    Added rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
 
     float speed = .1f;
+
+    float rotation = 0;
     void Start()
     {
         
@@ -20,13 +22,27 @@ public class PlayerController : MonoBehaviour
         updatePosition();
 
     }
+    public static Vector2 rotate(Vector2 v, float delta)
+    {
+        delta *= Mathf.Deg2Rad;
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
 
     private void updatePosition()
     {
-        Vector2 newPosition = transform.position;
 
-        newPosition.x -= speed * Input.GetAxis("Horizontal");
-        newPosition.y -= speed * Input.GetAxis("Vertical");
-        transform.position = newPosition;
+        Vector2 deltaVector = new Vector2(0,Input.GetAxis("Vertical"));
+
+        deltaVector = .2f*speed*rotate(deltaVector, rotation);
+
+        transform.Translate(deltaVector);
+
+        transform.Rotate(0,0,-1*Input.GetAxis("Horizontal"));
+                       
+        rotation += .2f * speed *Input.GetAxis("Horizontal");
+        Debug.Log(rotation);
     }
 }
